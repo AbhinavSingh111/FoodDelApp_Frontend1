0722df71a619739464ba0d9c92203af3bfb72141
Abhinav Singh <singh111abhinav@gmail.com>
2023-10-12 02:33:24 +0530

    min changes

--- index.js
+++ index.js
@@ -972,6 +972,10 @@ const BodyComponent = ()=>{
             </div>
             <div className="body-card">
                 {
+                    // we should use uuid as key rather than index , react oficially tells us not to.
+                    // because if the order of element gets changed because of a new addition of element
+                    // or some other reason the react might get confuse as to which property beloongs
+                    // to which component and this will affect performance negatively
                     data.map((restaurant)=>(<CardComponent key={restaurant.info.id} resObj={restaurant}/>))
                         
                 }
