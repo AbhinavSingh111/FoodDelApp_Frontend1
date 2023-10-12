273d4d3e2e3e966c09788cebb1f6cb237f7969e7
Abhinav Singh <singh111abhinav@gmail.com>
2023-10-12 18:44:44 +0530

    added filter feature m, used useState hook , added a new button css

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 * Never put hardcoded data like links or api data in components file
 * Put them in a sep file under folder name "utils" or "common". You can name the folder of file as you like ,  heare i am keeping file name as "constants" and "mockdata".
 * It is a good practice to write all the components in uppper case;
+
+* we can do 2 types of export -> default (single export) and named (multiple exports)
+* also there are 2 types of imports -> defaulet and named (put the name in {})
+    * eg import {XYX} from ../../utils
--- src/components/BodyComponent.js
+++ src/components/BodyComponent.js
@@ -1,13 +1,22 @@
 import SearchComponent from "./SearchComponent";
 import CardComponent from "./CardComponent";
 import data from "../../utils/mockData";
+import { useState } from "react";
 
 const BodyComponent = ()=>{
+    // using useState hook to bind data and ui
+    const [resList , setResList] = useState(data); 
     return (
         <>
         <div className="body">
             <div className="body-search">
                 <SearchComponent />
+            </div>
+            <div className="fltr">
+                <button className="fltr-btn" onClick={()=>{
+                const newList = resList.filter((res)=>res.info.avgRating>4);
+                setResList(newList);
+            }}>Filter Top</button>
             </div>
             <div className="body-card">
                 {
@@ -15,7 +24,7 @@ const BodyComponent = ()=>{
                     // because if the order of element gets changed because of a new addition of element
                     // or some other reason the react might get confuse as to which property beloongs
                     // to which component and this will affect performance negatively
-                    data.map((restaurant)=>(<CardComponent key={restaurant.info.id} resObj={restaurant}/>))
+                    resList.map((restaurant)=>(<CardComponent key={restaurant.info.id} resObj={restaurant}/>))
                         
                 }
                 
