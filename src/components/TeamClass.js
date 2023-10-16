f3d98cb0f0226384f2f678dc7eb650be2b2678dc
Abhinav Singh <singh111abhinav@gmail.com>
2023-10-16 13:10:00 +0530

    implenented state vars updation in CBCs

--- src/components/TeamClass.js
+++ src/components/TeamClass.js
@@ -5,12 +5,33 @@ class TeamComponent extends React.Component{
     constructor(props){
         // we use super method to call the constructor of the base/parent class to initialize the class with values passed
         super(props);
+
+        // defining state vars. They are defined as an obj , sate is a reserved keyword here
+        this.state = {
+            count1 : 0,
+            count2: 1,
+        }
     }
     // we use a render method which will return a jsx
     render(){
         const {name , location , contact} = this.props;
+
+        // destructuring state vars , we can use them directly also
+        const { count1 , count2} = this.state;
         return(
             <div className="team-card">
+                <h2>{count1}</h2>
+                <button onClick={()=>{
+                    this.setState({
+                        count1:this.state.count1+1,
+                    });
+                }}>Increase count1</button>
+                <h2>{this.state.count2}</h2>
+                <button onClick={()=>{
+                    this.setState({
+                        count2:this.state.count2+1,
+                    });
+                }}>Increase count2</button>
                 <h3 className="team-name">{name}</h3>
                 <h4 className="team-location">{location}</h4>
                 <h4 className="team-contact">{contact}</h4>
