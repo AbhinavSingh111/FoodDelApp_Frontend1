9cfaf219e0db5662bc5bb886e7b38d49528a2120
Abhinav Singh <singh111abhinav@gmail.com>
2023-11-06 19:46:51 +0530

    updated gitignore

--- src/components/CartShimmer.js
+++ src/components/CartShimmer.js
@@ -1,11 +1,11 @@
-
+import { CART_SHIMMER_IMAGE } from "../../utils/constants";
 const CartShimmer = ()=>{
     return (
         <div  className="text-center p-2 m-2 h-screen border-gray border-2 rounded-lg shadow-md bg-slate-50 relative ">
 
                 <p className="absolute t-0 l-0  mt-2 p-2 w-full text-slate-500 text-2xl">Your plate is empty...</p>
 
-                <img className="h-[100%]" src="https://assets.materialup.com/uploads/66fb8bdf-29db-40a2-996b-60f3192ea7f0/preview.png" />
+                <img className="h-[100%]" src={CART_SHIMMER_IMAGE}/>
     
         </div>
     )
--- utils/constants.js
+++ utils/constants.js
@@ -4,3 +4,4 @@ export const LOGO_IMG_LINK = "https://w7.pngwing.com/pngs/698/737/png-transparen
 // export const LOGO_IMG_LINK = "https://png.pngtree.com/png-vector/20220327/ourmid/pngtree-motorcycles-cartoon-pizza-and-food-delivery-instant-online-png-image_4517042.png";
 export const LIVE_DATA_LINK = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.8943893&lng=81.0368211&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 export const LIVE_MENU_LINK = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.876313600000024&lng=81.02215679999999&restaurantId=";
+export const CART_SHIMMER_IMAGE = "https://assets.materialup.com/uploads/66fb8bdf-29db-40a2-996b-60f3192ea7f0/preview.png";
